2e0043b489bb145f52027a6bf85d9e914f434fe8
psielachowicz <jaguar6@wp.pl>
2014-06-12 16:27:11 +0200

    spec
    sp

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,13 +1,13 @@
-require 'spec_helper'
+#require 'spec_helper'
 
-describe HomeController do
+#describe HomeController do
 
-  describe "GET 'index'" do
-    it "returns http success" do
+  #describe "GET 'index'" do
+    #it "returns http success" do
    
-     get 'index'
-     expect(response).to be_success
-    end
-  end
+    # get 'index'
+   #  expect(response).to be_success
+  #  end
+ # end
 
-end
+#end
